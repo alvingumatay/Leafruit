15c730e7bfd46aecea0147e1d0f3f4114d53194f
alvingumatay <alvingumatay13@gmail.com>
2023-06-04 23:59:32 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,259 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Leafruity | &copy 2023</title>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE-edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="author" content="Alvin T. Gumatay">
+  <link rel = "shortcut icon" href = "./images/logo.png" />
+  <link rel="stylesheet" type="text/css" href="./css/style.css">
+  <link href="./font-awesome-4.7.0/css/font-awesome.css" rel="stylesheet">	
+  <link href="./css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+  <!-------- Navbar-------->
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-success fixed-top" id="navbar">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Leafruity <img src="./images/logo.png"style="width: 50px; height: 50px; border-radius: 50%;"></a>
+
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav mx-auto">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="#">About Us</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Products</a>
+         </li>
+         <li class="nav-item">
+          <a class="nav-link active" href="#">Contact Us</a>
+        </li>
+      </ul>
+      <div class="d-grid gap-2">
+      <button class="btn btn-primary btn-block">Sign In</button>
+    </div>
+    </div>
+  </div>
+</nav>
+
+  <!--- end break ----------->
+
+  <!-------content HOME--------------->
+   <section id="home">
+     <h1>ORGANIC FRUIT PLANTS</h1>
+     <p>Ssles of variety's of plants inside of the bottle with a water.</p>
+   </section>
+
+
+  <!--- end break ----------->
+  
+  <!-------content ABOUT US--------------->
+   
+   <section id="about">
+     <div class="container-fluid">
+        <div class="row">
+           <div class="col-lg-6 col-md-6 col-12 p-lg-5 p-2 my-5">
+                <img src="./images/download.jpg" class="img-fluid">
+           </div>
+           <div class="col-lg-6 col-md-6 col-12 p-lg-5 p-2 my-5">
+                <h1>ABOUT US</h1>
+                <br/>
+                <p>The Leafruity is best of the online plant seller, now a day,
+                   we provide the organic plants that made in home. It is a fruit that grow in a plastic bottle and many different kinds of fruit plant.
+                   We offer to the customers are welcome and buy want kind of plant are want.  </p>
+           </div>
+        </div>
+     </div>
+   </section>
+
+
+  <!--- end break ----------->
+
+
+  <!-------content OUR PRODUCTS--------------->
+    <section id="products">
+      <div class="container m-5">
+           <h1 class="text-center my-5">OUR PRODUCTS</h1>
+           <div class="row gx-5">
+               <!--  <img src="./images/icon1.png"> -->
+           
+               <div class="col-md-4  col-md-2 col-12">
+                 <div class="img-back">
+                 <div class="card"> 
+                   <div class="card-body text-center">
+                    <img src="./images/plant3.jpg" class="card-img-top">
+                     <h5 class="card-title">Card Title</h5>
+                     <p class="card-text">Product Item 1</p>
+                     <a href="#" class="btn btn-success">Add To Cart</a>
+                   </div>
+                 </div>
+               </div> 
+             </div>
+             
+               <div class="col-md-4   col-md-2 col-12">
+                 <div class="img-back">
+                 <div class="card">
+                 
+                   <div class="card-body text-center">
+                    <img src="./images/plant9.png" class="card-img-top" style="height: 240px;">
+                     <h5 class="card-title">Card Title</h5>
+                     <p class="card-text">Product Item 2</p>
+                     <a href="#" class="btn btn-success">Add To Cart</a>
+                   </div>
+                 </div>
+               </div> 
+               </div>
+   
+               <div class="col-md-4 col-md-2 col-12">
+                <div class="img-back">
+                 <div class="card">
+                  <!--  <img src="./images/plant6.jpg"> -->
+                   <div class="card-body text-center">
+                    <img src="./images/plant6.jpg" class="card-img-top">
+                     <h5 class="card-title">Card Title</h5>
+                     <p class="card-text">Product Item 3</p>
+                     <a href="#" class="btn btn-success">Add To Cart</a>
+                   </div>
+                 </div>
+               </div> 
+             </div>
+           </div>
+
+            <div class="row gx-5">
+               <!--  <img src="./images/icon1.png"> -->
+           
+               <div class="col-md-4  col-md-2 col-12">
+                 <div class="img-back">
+                 <div class="card"> 
+                   <div class="card-body text-center">
+                    <img src="./images/plant3.jpg" class="card-img-top">
+                     <h5 class="card-title">Card Title</h5>
+                     <p class="card-text">Product Item 1</p>
+                     <a href="#" class="btn btn-success">Add To Cart</a>
+                   </div>
+                 </div>
+               </div> 
+             </div>
+             
+               <div class="col-md-4   col-md-2 col-12">
+                 <div class="img-back">
+                 <div class="card">
+                 
+                   <div class="card-body text-center">
+                    <img src="./images/plant9.png" class="card-img-top" style="height: 240px;">
+                     <h5 class="card-title">Card Title</h5>
+                     <p class="card-text">Product Item 2</p>
+                     <a href="#" class="btn btn-success">Add To Cart</a>
+                   </div>
+                 </div>
+               </div> 
+               </div>
+   
+               <div class="col-md-4 col-md-2 col-12">
+                <div class="img-back">
+                 <div class="card">
+                  <!--  <img src="./images/plant6.jpg"> -->
+                   <div class="card-body text-center">
+                    <img src="./images/plant6.jpg" class="card-img-top">
+                     <h5 class="card-title">Card Title</h5>
+                     <p class="card-text">Product Item 3</p>
+                     <a href="#" class="btn btn-success">Add To Cart</a>
+                   </div>
+                 </div>
+               </div> 
+             </div>
+           </div>
+
+            <div class="row gx-5">
+               <!--  <img src="./images/icon1.png"> -->
+           
+               <div class="col-md-4  col-md-2 col-12">
+                 <div class="img-back">
+                 <div class="card"> 
+                   <div class="card-body text-center">
+                    <img src="./images/plant3.jpg" class="card-img-top">
+                     <h5 class="card-title">Card Title</h5>
+                     <p class="card-text">Product Item 1</p>
+                     <a href="#" class="btn btn-success">Add To Cart</a>
+                   </div>
+                 </div>
+               </div> 
+             </div>
+             
+               <div class="col-md-4   col-md-2 col-12">
+                 <div class="img-back">
+                 <div class="card">
+                 
+                   <div class="card-body text-center">
+                    <img src="./images/plant9.png" class="card-img-top" style="height: 240px;">
+                     <h5 class="card-title">Card Title</h5>
+                     <p class="card-text">Product Item 2</p>
+                     <a href="#" class="btn btn-success">Add To Cart</a>
+                   </div>
+                 </div>
+               </div> 
+               </div>
+   
+               <div class="col-md-4 col-md-2 col-12">
+                <div class="img-back">
+                 <div class="card">
+                  <!--  <img src="./images/plant6.jpg"> -->
+                   <div class="card-body text-center">
+                    <img src="./images/plant6.jpg" class="card-img-top">
+                     <h5 class="card-title">Card Title</h5>
+                     <p class="card-text">Product Item 3</p>
+                     <a href="#" class="btn btn-success">Add To Cart</a>
+                   </div>
+                 </div>
+               </div> 
+             </div>
+           </div>
+
+
+        </div>
+    </section>
+  <!--- end break -----------> 
+
+  <!-------content Contact US--------------->
+   <section id="contact">
+        <div class="container">
+           <div class="row">
+             <div class="col-lg-6 col-md-6 col-12">
+                <img src="./images/plant1.jpg">
+             </div>
+             <div class="col-lg-6 col-md-6 col-12">
+                 <h3>CONTACT US</h3>
+                 <p>Cell No. #09462886584 Tnt <br/> 
+                            &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; #09390959176 Smart. </p>
+
+                            <h3> FOLLOW US or Email at </h3>
+           <p>leffruity@gmail.com </p>
+           <a href="#"><i class="fa fa-facebook"></i></a>
+           <a href="#"><i class="fa fa-twitter"></i></a>
+           <a href="#"><i class="fa fa-linkedin"></i></a>
+           <a href="#"><i class="fa fa-pinterest"></i></a>
+             </div>
+             
+           </div>
+        </div>
+    </section>
+
+  <!--- end break ----------->
+
+
+  <!----Footer -->
+
+<footer>Copyright @Leafruity &copy 2023</footer>
+   <!--- end break ----------->
+</body>
+<script type="text/javascript" src="./js/script.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"></script>
+</html>
